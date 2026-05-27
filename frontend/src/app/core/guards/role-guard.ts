437ba9d343716e07c6e62b02922e66bc057eda3d
michelangelo stega <mikybeeh@hotmail.it>
2026-05-27 22:09:18 +0200

    feat(frontend): implement authGuard and roleGuard
    - authGuard: checks isLoggedIn(), redirects to /login if not authenticated
    - roleGuard: checks isLoggedIn(), reads required roles from route.data, redirects to /dashboard (temporary) if role not allowed

--- frontend/src/app/core/guards/role-guard.ts
+++ frontend/src/app/core/guards/role-guard.ts
@@ -1,5 +1,24 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
 
 export const roleGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  //step 1: verifico se l'utente è autenticato, se non lo è, reindirizzo alla pagina di login
+  if (!authService.isLoggedIn()) {
+    return router.createUrlTree(['/login']); // reindirizza alla pagina di login se non autenticato
+  }
+  
+  //step 2: leggo i ruoli richiesti dalla route e quelli dell'utente
+  const requiredRoles = route.data['roles'];
+  const userRole = authService.getRole();
+
+  //step 3: verifico se l'utente ha almeno uno dei ruoli richiesti, se non li ha, reindirizzo alla pagina di accesso negato
+  if (requiredRoles && !userRole) {
+    return router.createUrlTree(['/dashboard']); // TODO: redirect temporaneo alla dashboard, da sostituire con una pagina di accesso negato
+  }
+
   return true;
 };
